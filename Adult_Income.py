60aca4082fc3db2de9b8e7555b0bfdc41697cd70
Ghanim <galghanim1999@gmail.com>
2021-11-04 00:07:04 +0300

    Update Adult_Income.py

--- Adult_Income.py
+++ Adult_Income.py
@@ -26,7 +26,7 @@ df['hours per week'].replace(99, np.mean(df['hours per week'].values), inplace=T
 
 df["income"].value_counts()/len(df["income"])
 
-# Mapping
+#%% Mapping
 
 df["gender"] = df["gender"].map({'Female':0, 'Male':1})
 
@@ -63,11 +63,12 @@ df
 df[['education', 'educational-num']].groupby(['education'], as_index=False).mean().sort_values(by='educational-num', ascending=False)
 
 df.drop(["education"], axis=1, inplace=True)
-df
 
 df.info()
 
 df.isnull().sum()
+
+#%%
 
 X = df.drop(["income"], axis=1)
 y = df["income"]
@@ -97,7 +98,6 @@ def evaluation(model, X_train, y_train, X_test, y_test, is_NN=False):
         y_pred = np.array(list(map(int, y_pred > 0.5)))
 
     print("Best model esitmator", model)
-    print(classification_report(y_test, y_pred, target_names = ['Under 50k', 'Over 50k']))
 
     if not is_NN:
         print('Training Set Accuracy Score: {:.2f}'.format(model.score(X_train, y_train)))
@@ -116,6 +116,25 @@ def confusion(y_true, y_pred):
 
 from sklearn.linear_model import LogisticRegression
 
+
+#%% SMOTE
+
+from imblearn.over_sampling import SMOTE
+
+sm = SMOTE(random_state=0)
+X_up_train, y_up_train = sm.fit_resample(X_train, y_train)
+
+y_up_train.value_counts()
+
+#%% normalize the data
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+X_up_train = scaler.fit_transform(X_up_train)
+X_test = scaler.transform(X_test)
+
+#%% Log Reg
 def logistic_regression(X_train, y_train):
     logreg = LogisticRegression()
     grid_values = {'penalty' : ['l1', 'l2'],
@@ -127,144 +146,15 @@ def logistic_regression(X_train, y_train):
 
     return grid_lr_rec.best_estimator_
 
-# Log Reg
-
-logreg = logistic_regression(X_train, y_train)
-y_pred = evaluation(logreg, X_train, y_train, X_test, y_test, is_NN=False)
-confusion(y_test, y_pred)
-
-# Random Forest
-
-from sklearn.ensemble import RandomForestClassifier
-
-def random_forest(X_train, y_train):
-    clf = RandomForestClassifier(random_state=0)
-
-    grid_values = {'max_depth': np.arange(1,11,2),
-                   'max_features': np.arange(1,11,2)}
-
-    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
-    grid_clf.fit(X_train, y_train)
 
 
-    return grid_clf.best_estimator_
-
-random_for = random_forest(X_train, y_train)
-y_pred = evaluation(random_for, X_train, y_train, X_test, y_test, is_NN=False)
-confusion(y_test, y_pred)
-
-# KNN
-
-from sklearn.neighbors import KNeighborsClassifier
-
-def knn(X_train, y_train):
-    clf = KNeighborsClassifier()
-
-    grid_values = {'n_neighbors': [23, 25, 35]}
-
-    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
-    grid_clf.fit(X_train, y_train)
-
-
-    return grid_clf.best_estimator_
-
-knn_clr = knn(X_train, y_train)
-y_pred = evaluation(knn_clr, X_train, y_train, X_test, y_test, is_NN=False)
-confusion(y_test, y_pred)
-
-# SVM
-
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, test_size=0.2, random_state=0)
-
-from sklearn.preprocessing import MinMaxScaler
-
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
-
-from sklearn.svm import SVC
-
-def svm(X_train, y_train):
-    clf = SVC(class_weight={1: 3})
-
-    #grid_values = {'kernel': ['rbf', 'linear'],
-    #               'C': [1, 0.8],
-    #               'degree': [1, 2]}
-    # take much time
-    grid_values = {}
-
-    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
-    grid_clf.fit(X_train, y_train)
-
-
-    return grid_clf.best_estimator_
-
-svm_clr = svm(X_train, y_train)
-y_pred = evaluation(svm_clr, X_train, y_train, X_test, y_test, is_NN=False)
-confusion(y_test, y_pred)
-
-# SMOTE
-
-from imblearn.over_sampling import SMOTE
-
-sm = SMOTE(random_state=0)
-X_up_train, y_up_train = sm.fit_resample(X_train, y_train)
-
-y_up_train.value_counts()
-
-from sklearn.preprocessing import MinMaxScaler
-
-scaler = MinMaxScaler()
-X_up_train = scaler.fit_transform(X_up_train)
-X_test = scaler.transform(X_test)
-
-from sklearn.ensemble import AdaBoostClassifier
-
-def ada_boost(X_train, y_train):
-    clf = AdaBoostClassifier()
-
-    grid_values = {'n_estimators': [50, 70,100]}
-
-    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
-    grid_clf.fit(X_train, y_train)
-
-
-    return grid_clf.best_estimator_
-
-ada_boost_clr = ada_boost(X_up_train, y_up_train)
-y_pred = evaluation(ada_boost_clr, X_up_train, y_up_train, X_test, y_test, is_NN=False)
-confusion(y_test, y_pred)
-
-# Gradient Boost
-
-from sklearn.ensemble import GradientBoostingClassifier
-
-def gradient_boost(X_train, y_train):
-    clf = GradientBoostingClassifier()
-
-    grid_values = {'n_estimators': [50, 70,100]}
-
-    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
-    grid_clf.fit(X_train, y_train)
-
-
-    return grid_clf.best_estimator_
-
-grad_boost_clr = gradient_boost(X_up_train, y_up_train)
-y_pred = evaluation(grad_boost_clr, X_up_train, y_up_train, X_test, y_test, is_NN=False)
+logreg = logistic_regression(X_up_train, y_up_train)
+y_pred = evaluation(logreg, X_up_train, y_up_train, X_test, y_test, is_NN=False)
 confusion(y_test, y_pred)
 
 
-grad_boost_clr = GradientBoostingClassifier(n_estimators=200, learning_rate=0.1, min_samples_split=500, min_samples_leaf=50,
-                                            max_depth=8, max_features='sqrt', subsample=0.8, random_state=42)
 
-grad_boost_clr.fit(X_up_train, y_up_train)
-y_pred = evaluation(grad_boost_clr, X_up_train, y_up_train, X_test, y_test, is_NN=False)
-confusion(y_test, y_pred)
-
-# Decision Tree
+#%% Decision Tree
 
 from sklearn.tree import DecisionTreeClassifier
 
@@ -283,17 +173,49 @@ rad_boost_clr = decision_tree(X_up_train, y_up_train)
 y_pred = evaluation(grad_boost_clr, X_up_train, y_up_train, X_test, y_test, is_NN=False)
 confusion(y_test, y_pred)
 
-from sklearn.tree import DecisionTreeClassifier
-dtree = DecisionTreeClassifier()
-dtree.fit(X_train,y_train)
-predictions = dtree.predict(X_test)
-accuracy_score = (y_test,predictions)
-print(classification_report(y_test,predictions))
 
-# cross val?
+#%% Random Forest
+
+from sklearn.ensemble import RandomForestClassifier
+
+def random_forest(X_train, y_train):
+    clf = RandomForestClassifier(random_state=0)
+
+    grid_values = {'max_depth': np.arange(1,11,2),
+                   'max_features': np.arange(1,11,2)}
+
+    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
+    grid_clf.fit(X_train, y_train)
 
 
-### cross validation
+    return grid_clf.best_estimator_
+
+random_for = random_forest(X_up_train, y_up_train)
+y_pred = evaluation(random_for, X_up_train, y_up_train, X_test, y_test, is_NN=False)
+confusion(y_test, y_pred)
+
+#%% KNN
+
+from sklearn.neighbors import KNeighborsClassifier
+
+def knn(X_train, y_train):
+    clf = KNeighborsClassifier()
+
+    grid_values = {'n_neighbors': [23, 25, 35]}
+
+    grid_clf = GridSearchCV(clf, param_grid = grid_values, scoring ='accuracy', cv=5)
+    grid_clf.fit(X_train, y_train)
+
+
+    return grid_clf.best_estimator_
+
+knn_clr = knn(X_train, y_train)
+y_pred = evaluation(knn_clr, X_train, y_train, X_test, y_test, is_NN=False)
+confusion(y_test, y_pred)
+
+#%% cross val
+
+
 from sklearn.model_selection import KFold #for K-fold cross validation
 from sklearn.model_selection import cross_val_score #score evaluation
 from sklearn.model_selection import cross_val_predict #prediction
@@ -314,9 +236,7 @@ for i in models:
 models_dataframe=pd.DataFrame({'CV Mean':xyz,'Std':std},index=classifiers)
 models_dataframe
 
-# ROC comparison
-
-
+#%% ROC comparison
 from sklearn.metrics import plot_roc_curve
 disp = plot_roc_curve(logreg, X_test, y_test)
 plot_roc_curve(dtree, X_test, y_test, ax=disp.ax_)
